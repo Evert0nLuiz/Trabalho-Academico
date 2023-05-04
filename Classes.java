f4a2ed7d3cbf6d29d7ef1877cad2e1a5f04cd2d2
Everton <76123738+Vzuviiu@users.noreply.github.com>
2023-05-04 22:05:36 +0000

    Testes dia 04-05-2023

---
+++ Classes.java
@@ -0,0 +1,17 @@
+public class Classes 
+{
+    
+    public void Soldado()
+    {
+        int vida = 140;
+        int força = 20;
+        int furtividade = 3;
+        int inteligencia = -12;
+        System.out.println();
+
+    }
+
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+import Classes;
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        Classes escolha = new Classes();
+         escolhaJoj = escolha.Soldado();
+
+
+        System.out.println(escolha);
+    }
+}
