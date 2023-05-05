fee3eb7c2e20342606f0f5dcf4a9a1da5e7f347b
Everton <76123738+Vzuviiu@users.noreply.github.com>
2023-05-05 23:51:45 +0000

    Classe para adicionar mecanicas de jogo

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
-import Classes;
+public class Main extends MecanicasDoJogo{
 
-public class Main
-{
-    public static void main(String [] args)
-    {
-        Classes escolha = new Classes();
-         escolhaJoj = escolha.Soldado();
+    public static void main(String[] args) {
+        MecanicasDoJogo cientista = new MecanicasDoJogo();
+        int vida;
+        int inteligencia;
+        int furtividade;
+        int forca;
+
+        cientista.setClasseSoldado();
+
+        vida = cientista.getVida();
+        forca = cientista.getForca();
+        inteligencia = cientista.getInteligencia();
+        furtividade = cientista.getFurtividade();
+
+        vida = cientista.heal(22);
 
 
-        System.out.println(escolha);
+        System.out.println("Sua vida atual: " + vida + "\nForça: " + forca + " \nInteligência: " + inteligencia + "\nFurtividade: " + furtividade);
     }
 }
+
+
---
+++ MecanicasDoJogo.java
@@ -0,0 +1,43 @@
+public class MecanicasDoJogo 
+{
+    int vida;
+    int furtividade;
+    int forca;
+    int inteligencia;
+    String [] mochila;
+    public void setClasseSoldado()
+    {
+        vida = 100;
+        inteligencia = 100;
+        forca = 100;
+        furtividade = 100;
+
+
+    }
+    public int getVida(){
+        return vida;
+
+    }
+    public  int getInteligencia(){
+        return inteligencia;
+    }
+    public int getFurtividade(){
+        return furtividade;     
+    }
+    public int getForca(){
+        return forca;     
+    }
+
+
+
+
+    public int damage(int dano){
+        vida = vida - dano;
+
+        return vida;
+    }
+ 
+   
+
+
+}
