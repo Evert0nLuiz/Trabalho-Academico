f4a1017f56ac1834ec624f322d50b071982746a3
Vzuviiu <evertonluizdacruzsousa@hotmail.com>
2023-05-16 20:35:27 -0300

    atualização dia 16/05/2023 para fazer funcionar

---
+++ com.trabalhoPi.rpgaliveindaed/README.md
@@ -0,0 +1,7 @@
+# Trabalho-Academico
+
+
+Jogo proposto como projeto na materia de Projeto Integrador do curso de Analise e Desenvolvimento de Sistemas na faculdade Serviço Nacional de Aprendizagem comercial.
+Um RPG, por enquanto sem nome.
+
+Para meus colegas de grupo peço apenas que crie sua branch para fazer seu código, faça o commit e faça a pull request após terminar, o restante do grupo irá testar, comentar se aprovaram e iniciarei o merge. Obrigado
---
+++ com.trabalhoPi.rpgaliveindaed/Testes.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.rpgaliveindead;
+
+/**
+ *
+ * @author everton.lcsousa
+ */
+public class Testes extends MecanicasDoJogo{
+
+    /**
+     * Teste de metodos da classe mecanica
+     * 
+     */
+    public static void main(String[] args) {
+        MecanicasDoJogo cientista = new MecanicasDoJogo();
+        int vida;
+        int inteligencia;
+        int furtividade;
+        int forca;
+        cientista.setClasseSoldado();
+
+        vida = cientista.getVida();
+        forca = cientista.getForca();
+        inteligencia = cientista.getInteligencia();
+        furtividade = cientista.getFurtividade();
+        
+
+        System.out.println("Sua vida atual: " + vida + "\nForÃ§a: " + forca + " \nInteligÃªncia: " + inteligencia + "\nFurtividade: " + furtividade);
+        cientista.adicionarItensNaMochila("Chave");
+        
+        cientista.mostrarMochila();
+        
+        cientista.adicionarItensNaMochila("Arma");
+
+        cientista.mostrarMochila();
+       
+      
+    }
+}
+
+
