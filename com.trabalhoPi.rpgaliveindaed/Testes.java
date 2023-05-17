c4d9f4f95ddb6d05f1891dc2e1896cda7f220ab5
Everton <76123738+Evert0nLuiz@users.noreply.github.com>
2023-05-16 22:20:39 -0300

    Update Testes.java

--- com.trabalhoPi.rpgaliveindaed/Testes.java
+++ com.trabalhoPi.rpgaliveindaed/Testes.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.trabalhoPi.rpgaliveindead;
+package com.trabalhopi.rpgaliveindead;
 
 /**
  *
